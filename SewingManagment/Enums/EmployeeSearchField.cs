405059e1489f4205799f178f1a31c0468061f69f
zenLabX <simple.forwork2025@gmail.com>
2025-10-14 13:47:52 +0800

    feat: 封裝下拉選單元件

---
+++ SewingManagment/Enums/EmployeeSearchField.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SewingManagment.Enums
+{
+    public enum EmployeeSearchField
+    {
+        [Display(Name = "姓名")]
+        Name,
+        [Display(Name = "職務")]
+        Position,
+        [Display(Name = "性別")]
+        Gender
+    }
+}
---
+++ SewingManagment/Extensions/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace SewingManagment.Extensions
+{
+    public static class EnumExtensions
+    {
+        /** 下拉選單專用 */
+        public static IEnumerable<SelectListItem> ToSelectList<TEnum>(this TEnum enumObj)
+            where TEnum : Enum
+        {
+            return Enum.GetValues(typeof(TEnum))
+                       .Cast<TEnum>()
+                       .Select(e => new SelectListItem
+                       {
+                           Text = e.GetType()
+                                   .GetMember(e.ToString())
+                                   .First()
+                                   .GetCustomAttribute<DisplayAttribute>()?.Name
+                                   ?? e.ToString(),
+                           Value = e.ToString()
+                       });
+        }
+    }
+}
