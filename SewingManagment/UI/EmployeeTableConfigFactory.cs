2ba2cdf7c75afa87f1fcdb4991b48c7a98590cf0
zenLabX <simple.forwork2025@gmail.com>
2025-10-15 10:43:59 +0800

    fix: 1.移除註解 2.修正編輯按鈕、下拉選單搜尋欄位

--- SewingManagment/UI/EmployeeTableConfigFactory.cs
+++ SewingManagment/UI/EmployeeTableConfigFactory.cs
@@ -17,6 +17,7 @@ namespace SewingManagment.UI
                     { "Gender", "性別" },
                     { "Position", "職務" }
                 },
+                // 可根據資料客製化 FormatFunc
                 FormatFunc = (item, col) =>
                 {
                     var e = (Employee)item;
@@ -33,8 +34,11 @@ namespace SewingManagment.UI
                     };
                 },
                 RowClassFunc = e => e.Position == "03" ? "table-warning" : "",
+                // Note: Tag helpers (asp-*) are not processed when emitting HTML from a string in code.
+                // Generate normal href links so the Edit button actually navigates.
+                // 需要手動改跳轉到指定頁
                 ActionsFunc = e => new HtmlString($@"
-                <a class='btn btn-warning px-4' asp-controller='Employee' asp-action='Edit' asp-route-id='{e.Id}'>編輯</a>
+                <a class='btn btn-warning px-4' href='/Employee/Edit/{e.Id}'>編輯</a>
                 <a class='btn btn-danger px-4 btn-delete' data-id='{e.Id}'>刪除</a>")
             };
         }
